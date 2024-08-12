84e85e8234cd3392405e0142a3292bcc42935688
tjasz <nukorausa@hotmail.com>
2024-08-11 18:04:14 -0700

    Start moon phases cheat sheet

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,52 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import phases from './phases.json'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Moon Phase Cheat Sheet</h1>
+      <table>
+        <tbody>
+          <tr>
+            <th>Length of orbit:</th>
+            <td>27.322d</td>
+          </tr>
+          <tr>
+            <th>Length of sidereal day:</th>
+            <td>29.5d</td>
+          </tr>
+        </tbody>
+      </table>
+      {phases.map(phase => (<div className="phase">
+        <h2>{phase.name}</h2>
+        <img src={`img/${phase.imgSrc}.jpg`} />
+        <table>
+          <tbody>
+            <tr>
+              <th>Rises:</th>
+              <td>{phase.rises}</td>
+            </tr>
+            <tr>
+              <th>Sets:</th>
+              <td>{phase.sets}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>))}
+      <div id="footnotes">
+        <ol>
+          <li id="source1">
+            <a target="_blank" href="https://science.nasa.gov/moon/top-moon-questions/">
+              Top Moon Questions - NASA Science
+            </a>
+            <a target="_blank" href="https://science.nasa.gov/moon/moon-phases/">
+              Moon Phases - NASA Science
+            </a>
+          </li>
+        </ol>
+      </div>
     </div>
   );
 }
